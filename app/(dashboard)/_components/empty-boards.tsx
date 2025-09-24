e4a2e308ef0de6b63dc912ac007d7c6dd48416e4
Jatin Sharma <jatts0010@gmail.com>
2025-09-24 23:38:35 +0530

    added boards table schema and create board api function in convex

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,25 @@
-import { Button } from '@/components/ui/button';
+'use client';
+
 import Image from 'next/image';
+import { useOrganization } from '@clerk/nextjs';
+
+import { api } from '@/convex/_generated/api';
+import { Button } from '@/components/ui/button';
+import { useApiMutation } from '@/hooks/use-api-mutation';
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: 'Untitled',
+    });
+  };
+
   return (
     <div className='h-full flex flex-col items-center justify-center'>
       <Image src='/note.svg' alt='Empty' height={120} width={120} />
@@ -10,7 +28,9 @@ export const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className='mt-6'>
-        <Button size='lg'>Create Board</Button>
+        <Button disabled={pending} onClick={onClick} size='lg'>
+          Create Board
+        </Button>
       </div>
     </div>
   );
