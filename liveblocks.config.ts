8139b29f64a2d0f9ababf15748a34805356f621d
Jatin Sharma <jatts0010@gmail.com>
2025-09-28 17:39:58 +0530

    added loading components and skeletons for board interface; integrated room provider from liveblocks for collaborative functionality

---
+++ liveblocks.config.ts
@@ -0,0 +1,95 @@
+// Define Liveblocks types for your application
+// https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
+
+import { createClient } from '@liveblocks/client';
+import { createRoomContext } from '@liveblocks/react';
+
+// declare global {
+//   interface Liveblocks {
+// Each user's Presence, for useMyPresence, useOthers, etc.
+// Presence: {
+//   cursor: { x: number; y: number } | null;
+//   selection?: string[];
+// };
+
+// The Storage tree for the room, for useMutation, useStorage, etc.
+// Storage: {
+//   // Define your storage structure here
+//   layers?: unknown;
+//   layerIds?: unknown;
+// };
+
+// Custom user info set when authenticating with a secret key
+// UserMeta: {
+//   id: string;
+//   info: {
+//     name: string;
+//     picture: string;
+//   };
+// };
+
+// Custom events, for useBroadcastEvent, useEventListener
+// RoomEvent:
+//   | { type: 'SELECTION_CHANGE' }
+//   | { type: 'CURSOR_MOVE'; cursor: { x: number; y: number } };
+
+// Custom metadata set on threads, for useThreads, useCreateThread, etc.
+// ThreadMetadata: {
+//   x: number;
+//   y: number;
+// };
+
+// Custom room info set with resolveRoomsInfo, for useRoomInfo
+// RoomInfo: {
+//   title: string;
+//   url: string;
+// };
+//   }
+// }
+
+const client = createClient({
+  publicApiKey:
+    'pk_dev_aEMuHOb6NtNwOwdEQAnCDQXw3B4cxuOS6Qpa8fKiYCgycT2Pl71jDBNwaX8EmLrj',
+});
+
+// Create the RoomProvider and hooks
+export const {
+  suspense: {
+    RoomProvider,
+    useRoom,
+    useMyPresence,
+    useUpdateMyPresence,
+    useOthers,
+    useSelf,
+    useOthersMapped,
+    useOthersConnectionIds,
+    useOther,
+    useStorage,
+    useMutation,
+    useHistory,
+    useUndo,
+    useRedo,
+    useCanUndo,
+    useCanRedo,
+    useBroadcastEvent,
+    useEventListener,
+    useErrorListener,
+  },
+} = createRoomContext(client);
+
+// export {
+//   client,
+//   RoomProvider,
+//   useMyPresence,
+//   useOthers,
+//   useSelf,
+//   useStorage,
+//   useMutation,
+//   useHistory,
+//   useUndo,
+//   useRedo,
+//   useCanUndo,
+//   useCanRedo,
+//   useBroadcastEvent,
+//   useEventListener,
+// };
