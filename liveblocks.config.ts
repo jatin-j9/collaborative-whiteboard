80c0648247c5493a9bf04af5655b3264d9ffe3f0
Jatin Sharma <jatts0010@gmail.com>
2025-09-30 17:29:11 +0530

    implemented layer management and rendering; added LayerPreview and Rectangle components; updated canvas interactions for layer insertion

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,23 +1,29 @@
 // Define Liveblocks types for your application
 // https://liveblocks.io/docs/api-reference/liveblocks-react#Typing-your-data
 
-import { createClient } from '@liveblocks/client';
+import {
+  createClient,
+  LiveList,
+  LiveMap,
+  LiveObject,
+} from '@liveblocks/client';
 import { createRoomContext } from '@liveblocks/react';
+
+import { Layer, Color } from '@/types/canvas';
 
 declare global {
   interface Liveblocks {
     // Each user's Presence, for useMyPresence, useOthers, etc.
     Presence: {
       cursor: { x: number; y: number } | null;
-      // selection?: string[];
+      selection: string[];
     };
 
     // The Storage tree for the room, for useMutation, useStorage, etc.
-    // Storage: {
-    //   // Define your storage structure here
-    //   layers?: unknown;
-    //   layerIds?: unknown;
-    // };
+    Storage: {
+      layers: LiveMap<string, LiveObject<Layer>>;
+      layerIds: LiveList<string>;
+    };
 
     // Custom user info set when authenticating with a secret key
     UserMeta: {
