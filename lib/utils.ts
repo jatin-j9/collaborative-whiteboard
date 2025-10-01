fbd5d406d3245f81d0405e9a52ae96f456eea013
Jatin Sharma <jatts0010@gmail.com>
2025-10-01 11:06:47 +0530

    updated canvas interactions with layer selection and color indication; added util function for color conversion

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-import { Camera } from '@/types/canvas';
+import { Camera, Color } from '@/types/canvas';
 
 const COLORS = [
   '#e6194B',
@@ -32,4 +32,8 @@ export function pointerEventToCanvasPoint(
     x: Math.round(e.clientX - camera.x),
     y: Math.round(e.clientY - camera.y),
   };
+}
+
+export function colorToCss(color: Color) {
+  return `#${color.r.toString(16).padStart(2, '0')}${color.g.toString(16).padStart(2, '0')}${color.b.toString(16).padStart(2, '0')}`;
 }
